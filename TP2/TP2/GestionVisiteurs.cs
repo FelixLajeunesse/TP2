9082cca36cb19064eca92802fa227665b0243f3c
yoan jalbert <6294918@cstjean.qc.ca>
2025-09-25 11:33:32 -0400

    11:33

---
+++ TP2/TP2/GestionVisiteurs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP2
+{
+    public class GestionVisiteurs
+    {
+        public LinkedList<Visiteur> visiteursActuel { get; set; } = new LinkedList<Visiteur>();
+
+        public Queue<Visiteur> visiteursFileAttente { get; set; } = new Queue<Visiteur>();
+
+        public void EntrerVisiteurDansFileAttente(string attractionId, Visiteur visiteur)
+        {
+
+        }
+        public void EntrerVisiteurDansAttraction(string attractionId)
+        {
+
+        }
+        public void EntrerVisiteurDansParc(Visiteur visiteur)
+        { 
+            visiteursActuel.AddLast(visiteur);
+            visiteur.historique.Add(" - Entrez dans le parc.");
+        }
+        public void SortirVisiteurDuParc(Visiteur visiteur)
+        {
+            visiteursActuel.Remove(visiteur);
+            visiteur.historique.Add(" - Sortir du parc.");
+        }
+
+
+    }
+}
---
+++ TP2/TP2/Visiteur.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP2
+{
+    public class Visiteur
+    {
+        public string Nom { get; set; }
+
+        public List<string> historique { get; set; } = [];
+
+        public Visiteur(string nom)
+        {
+            Nom = nom;
+        }
+    }
+}
