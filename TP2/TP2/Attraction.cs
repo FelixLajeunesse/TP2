3c25d2b63682d5122828bfe0dc337c46809d5736
FelixLajeunesse <6310376@cstjean.qc.ca>
2025-09-25 11:00:10 -0400

    Ajout des classes Parc et Attraction

---
+++ TP2/TP2/Attraction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP2
+{
+    internal class Attraction
+    {
+        private int _id { get; set; }
+        private string _nom { get; set; }
+        private Type _type { get; set; }
+
+        public Attraction(int id, string nom, Type type)
+        { 
+            this._id = id;
+            this._nom = nom;
+            this._type = type;
+        }
+    }
+    enum Type { S, I, F, T, M, R }
+}
---
+++ TP2/TP2/Parc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP2
+{
+    public class Parc
+    {
+        private List<Attraction> _list = new List<Attraction>();
+
+        public Parc() { }
+
+    }
+}
