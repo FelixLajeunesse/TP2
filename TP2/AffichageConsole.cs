55d68312289868b6c25c558aacd45470c3a98193
yoan jalbert <6294918@cstjean.qc.ca>
2025-10-09 15:03:51 -0400

    id des attratcions en couleur plus ajout des cercles à la place des étoiles, et petit règlage pour que sa sois formater comme ce que le prof a fait

--- TP2/AffichageConsole.cs
+++ TP2/AffichageConsole.cs
@@ -3,6 +3,7 @@
 // </copyright>
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,11 +23,19 @@ namespace TP2
         /// <param name="gestionVisiteurs">Permet de faire la gestion des visiteurs.</param>
         public static void Afficher(Parc parc, Map map, GestionVisiteurs gestionVisiteurs)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
             for (int i = 0; i < map.Carte.GetLength(0); i++)
             {
                 for (int j = 0; j < map.Carte.GetLength(1); j++)
                 {
+                    if (map.Carte[i, j].Trim() != "-----")
+                    {
+                        Console.ForegroundColor = DeterminerCouleurAttraction(map.Carte[i, j].Trim(), gestionVisiteurs);
+                    }
+
                     Console.Write(map.Carte[i, j]);
+                    Console.ResetColor();
                 }
 
                 Console.WriteLine();
@@ -36,23 +45,11 @@ namespace TP2
 
             foreach (Attraction attraction in parc.Attractions.Values)
             {
-                if (gestionVisiteurs.AttractionsFileVisiteur[attraction.Id].Count == 3)
-                {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                }
-                else if (gestionVisiteurs.AttractionsFileVisiteur[attraction.Id].Count == 4)
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                }
-
-                Console.Write("* ");
+                Console.ForegroundColor = DeterminerCouleurAttraction(attraction.Id, gestionVisiteurs);
+                Console.Write("{0,2}", "\u2B24");
                 Console.ResetColor();
 
-                Console.WriteLine($"{attraction.Id,-10}   {attraction.Type,5}   {attraction.Nom,-30}     {gestionVisiteurs.AttractionsFileVisiteur[attraction.Id].Count,5} / {attraction.Capacite,1}");
+                Console.WriteLine($"{attraction.Id,7} {attraction.Nom,17} ({attraction.Type})  {gestionVisiteurs.AttractionsFileVisiteur[attraction.Id].Count,8} / {attraction.Capacite}");
             }
         }
 
@@ -67,6 +64,28 @@ namespace TP2
             for (int i = 0; i < visiteur.Historique.Count; i++)
             {
                 Console.WriteLine($"{visiteur.Historique[i]}.");
+            }
+        }
+
+        /// <summary>
+        /// Détermine la couleur d'affichage d'une attraction en fonction de son état.
+        /// </summary>
+        /// <param name="id">L'identifiant de l'attraction.</param>
+        /// <param name="gestionVisiteurs">La gestion des visiteurs.</param>
+        /// <returns>La couleur d'affichage de l'attraction.</returns>
+        public static ConsoleColor DeterminerCouleurAttraction(string id, GestionVisiteurs gestionVisiteurs)
+        {
+            if (gestionVisiteurs.AttractionsFileVisiteur[id].Count == 3)
+            {
+                return ConsoleColor.Yellow;
+            }
+            else if (gestionVisiteurs.AttractionsFileVisiteur[id].Count == 4)
+            {
+                return ConsoleColor.Red;
+            }
+            else
+            {
+                return ConsoleColor.Green;
             }
         }
     }
--- TP2/obj/Debug/net8.0/TP2.AssemblyInfo.cs
+++ TP2/obj/Debug/net8.0/TP2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9a7aa5ed0a9ac4d1f169169a8c42f106a95257b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d4d30cb9baf871351ff99d88982917e35cb2f11")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
